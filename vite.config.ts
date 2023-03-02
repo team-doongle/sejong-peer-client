a0c1c7d4a40610c18cb4e90d8e388d62f3ce0105
ggsno <ogs0518@gmail.com>
2023-03-02 23:41:00 +0900

    fix: 경로 인식 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import mkcert from "vite-plugin-mkcert";
-
+import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { https: true },
-  plugins: [react(), mkcert()],
+  plugins: [react(), mkcert(), tsconfigPaths()],
 });
