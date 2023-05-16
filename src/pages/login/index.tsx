7928d405608332a7152350f9ed44c83f2c7bed4d
ggsno <ogs0518@gmail.com>
2023-05-16 15:05:51 +0900

    feat: 로그인 폼 임시 삭제, 안내문 추가

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -9,12 +9,13 @@ export default function LoginPage() {
       <Loading />
       <div className="flex flex-col items-center justify-center h-[80vh]">
         <div className="flex flex-col items-center w-[300px]">
-          <ReleaseNotes />
+          {/* <ReleaseNotes /> */}
           <MainLogo />
           <div className="text-[20px] mt-[8px] mb-[32px] my-0">
             Sejong Peer !
           </div>
-          <LoginForm />
+          {/* <LoginForm /> */}
+          다음 학기에 다시 만나요 !
         </div>
       </div>
     </>
